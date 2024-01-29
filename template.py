f1783dce5a4b65e94d2b8a41cb0be2ad28f63e5a
Sakil786 <sakilansari4@gmail.com>
2024-01-30 01:15:48 +0530

    template.py

---
+++ template.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+list_of_files = [
+   ".github/workflows/.gitkeep",
+   "src/__init__.py",
+   "src/components/__init__.py",
+   "src/components/data_ingestion.py",
+   "src/components/data_transformation.py",
+   "src/components/model_trainer.py",
+   "src/components/model_evaluation.py",
+   "src/components/pipeline/__init__.py",
+   "src/components/pipeline/training_pipeline.py",
+   "src/components/pipeline/prediction_pipeline.py",
+   "src/utils/utils.py",
+   "tests/unit/__init__.py",
+   "tests/integration/__init__.py",
+   "inti_setup.sh",
+   "requirements.txt",
+   "requirements_dev.txt",
+   "setup.py",
+   "setup.config",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # create an empty file
